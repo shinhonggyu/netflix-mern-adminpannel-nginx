cf94d6529eee26880f744603b2cc139d2012ad3b
shinhonggyu <zowoz0@naver.com>
2021-09-07 12:10:36 +0900

    api auth complete

--- api/index.js
+++ api/index.js
@@ -3,16 +3,18 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const colors = require('colors');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(() => console.log('DB Connection Successfull'.cyan.bold))
   .catch((error) => console.log(error));
+
+app.use(express.json());
+
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
   console.log('Backend server is running'.green.bold);
